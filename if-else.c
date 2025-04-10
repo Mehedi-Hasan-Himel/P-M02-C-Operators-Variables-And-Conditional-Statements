a4abc55281a056b1bb6e31896eea3a0144b70ae8
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2025-04-10 18:46:36 +0600

    if else

---
+++ if-else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+
+    int tk;
+    scanf("%d", &tk);
+
+    if(tk >= 100){
+        printf("Burgar khabo\n");
+    }else{
+        printf("Burgar khabo na\n");
+    }
+
+    return 0;
+}
