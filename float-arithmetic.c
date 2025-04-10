3cd4c21d9e3ebd38a89a5598f008946aad02d7a0
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2025-04-10 17:56:26 +0600

    P-M2-summation substraction multiplication division of integer.

---
+++ float-arithmetic.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    float a =15;
+    int b = 2;
+
+    float div = a/b;
+    printf("%f",div);
+    return 0;
+}
---
+++ int-arichmatic.c
@@ -0,0 +1,25 @@
+// summation substraction multiplication division of integer. 
+#include <stdio.h>
+int main(){
+    int a = 10;
+    int b = 5;
+
+    // summation of 2 integer
+    int sum = a+b;  
+    printf("Sum = %d\n",sum);
+
+    // substraction of 2 integer
+    int sub = a-b;
+    printf("Sub = %d\n",sub);
+
+    // multiplication of 2 integer
+    int mul = a*b;
+    printf("mul = %d\n",mul);
+
+    // division of 2 integer
+    int div = a/b;
+    printf("div = %d\n",div);
+
+    return 0;
+}
+
